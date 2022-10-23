8fccb4d92f6155d63517644e122f3fdcd37d3531
Ruichen Zhang <ruichen199801@gmail.com>
2022-10-23 18:33:07 -0400

    add images

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FacebookSignin from './components/FacebookSignin';
-import GoogleSignin from './components/GoogleSignin';
 import Signin from './components/Signin';
-import Signup from './components/Signup';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<FacebookSignin />} />
-                {/* <Route path="/" element={<GoogleSignin />} /> */}
+                <Route path="/" element={<Signin />} />
                 <Route path="/signin" element={<Signin />} />
-                <Route path="/signup" element={<Signup />} />
             </Routes>
         </BrowserRouter>
     );
