d117861d3de971a133de5f266672305b531b1934
Ruichen Zhang <ruichen199801@gmail.com>
2022-11-28 17:57:37 -0500

    add analytics route

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,6 @@ import RestaurantPage from "components/Restaurant/RestaurantPage";
 import ReviewerPage from "components/Reviewer/ReviewerPage";
 import UserPage from "components/User/UserPage";
 import AnalyticsPage from "components/Analytics/AnalyticsPage";
-import RestaurantAnalyticsItem from "components/Analytics/RestaurantAnalyticsItem";
 
 const App = () => {
   return (
@@ -22,7 +21,6 @@ const App = () => {
         <Route path="/reviewer/:reviewerId" element={<ReviewerPage />} />
         <Route path="/user" element={<UserPage />} />
         <Route path="/analytics" element={<AnalyticsPage />} />
-        <Route path="/item" element={<RestaurantAnalyticsItem />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ routes/analyticsRoutes.js
@@ -0,0 +1,27 @@
+const db = require("../database");
+const requireLogin = require("../middlewares/requireLogin");
+
+const query = `
+SELECT restaurantName, avgRating
+FROM Restaurant t1
+         LEFT JOIN Review t2 ON t1.restaurantId = t2.restaurantId
+WHERE content LIKE '%steak%'
+  AND open = 'Y'
+GROUP BY t1.restaurantId
+HAVING AVG(rating) > 4
+   AND COUNT(t1.restaurantId) > 30
+ORDER BY AVG(rating) DESC
+LIMIT 5;
+`;
+
+module.exports = (app) => {
+  app.get("/api/recommend", requireLogin, (req, res) => {
+    db.query(query, (error, results, fields) => {
+      if (error) {
+        res.status(404).json({ error });
+      } else if (results) {
+        res.status(200).json({ results });
+      }
+    });
+  });
+};
