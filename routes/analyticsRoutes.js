9e224ab8faf6be9861f34167e1a4f8aa3e222071
Ruichen Zhang <ruichen199801@gmail.com>
2022-11-28 19:14:40 -0500

    finish restaurant route

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,21 +1,21 @@
 const db = require("../database");
 const requireLogin = require("../middlewares/requireLogin");
 
-const query = `
-SELECT restaurantName, avgRating
-FROM Restaurant t1
-         LEFT JOIN Review t2 ON t1.restaurantId = t2.restaurantId
-WHERE content LIKE '%steak%'
-  AND open = 'Y'
-GROUP BY t1.restaurantId
-HAVING AVG(rating) > 4
-   AND COUNT(t1.restaurantId) > 30
-ORDER BY AVG(rating) DESC
-LIMIT 5;
-`;
-
 module.exports = (app) => {
   app.get("/api/recommend", requireLogin, (req, res) => {
+    const query = `
+    SELECT restaurantName, avgRating
+    FROM Restaurant t1
+             LEFT JOIN Review t2 ON t1.restaurantId = t2.restaurantId
+    WHERE content LIKE '%steak%'
+      AND open = 'Y'
+    GROUP BY t1.restaurantId
+    HAVING AVG(rating) > 4
+       AND COUNT(t1.restaurantId) > 30
+    ORDER BY AVG(rating) DESC
+    LIMIT 5;
+    `;
+
     db.query(query, (error, results, fields) => {
       if (error) {
         res.status(404).json({ error });
