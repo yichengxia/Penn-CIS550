2e730c82a5bc2cf8f7fc9869b7a0149da6fe9c46
Ruichen Zhang <ruichen199801@gmail.com>
2022-12-01 09:10:18 -0500

    add 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import RestaurantPage from "components/Restaurant/RestaurantPage";
 import ReviewerPage from "components/Reviewer/ReviewerPage";
 import UserPage from "components/User/UserPage";
 import AnalyticsPage from "components/Analytics/AnalyticsPage";
+import NotFoundPage from "components/NotFound/NotFoundPage";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <Route path="/reviewer/:reviewerId" element={<ReviewerPage />} />
         <Route path="/user" element={<UserPage />} />
         <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
