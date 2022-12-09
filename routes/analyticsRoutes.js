a26c5d844d93f0a193fcfabe13b16d325dab392f
Ruichen Zhang <ruichen199801@gmail.com>
2022-12-09 15:50:10 -0500

    revert pull request to update sql queries as functional problems are detected with the best same city query

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -28,97 +28,59 @@ module.exports = (app) => {
       `;
 
       const bestInReviewContent = `
-      SELECT t1.restaurantId,
-             t1.restaurantName,
-             t1.avgRating,
-             t1.city,
-             RM.imageUrl
+      SELECT t1.restaurantId, restaurantName, avgRating, city, imageUrl
       FROM Restaurant t1
-               INNER JOIN
-           Review t2
-           ON t1.restaurantId = t2.restaurantId
-               JOIN
-           RestaurantMedia RM
-           ON t1.restaurantId = RM.restaurantId
-      WHERE t2.content LIKE '%steak%'
-        AND t1.open = 'Y'
+           LEFT JOIN Review t2 ON t1.restaurantId = t2.restaurantId
+           JOIN RestaurantMedia RM on t1.restaurantId = RM.restaurantId
+      WHERE content LIKE '%steak%'
+        AND open = 'Y'
       GROUP BY t1.restaurantId
-      HAVING AVG(t2.rating) > 4
-         AND COUNT(t1.restaurantId) > 30
-      ORDER BY AVG(t2.rating) DESC
-      LIMIT 5
+      HAVING AVG(rating) > 4
+        AND COUNT(t1.restaurantId) > 30
+      ORDER BY AVG(rating) DESC
+      LIMIT 5;
       `;
 
       const bestEachCity = `
-      WITH temp1 AS (SELECT t1.city,
-                 MAX(t2.usefulCount) AS max_count
-          FROM Restaurant t1
-                 JOIN
-            Review t2
-            ON t1.restaurantId = t2.restaurantId
-          GROUP BY t1.city
-          ORDER BY NULL)
-      SELECT t1.restaurantId,
-         t1.restaurantName,
-         t1.avgRating,
-         t1.city,
-         RM.imageUrl
+      WITH temp1 AS (SELECT city, MAX(usefulCount) AS max_count
+               FROM Restaurant t1
+                        JOIN Review t2
+                             ON t1.restaurantId = t2.restaurantId
+               GROUP BY city)
+      SELECT t1.restaurantId, restaurantName, avgRating, t1.city, imageUrl
       FROM Restaurant t1
-          JOIN
-        temp1 t2
-        ON t1.city = t2.city
-          JOIN
-        Review t3
-        ON t1.restaurantId = t3.restaurantId
-          JOIN
-        RestaurantMedia RM
-        ON t1.restaurantId = RM.restaurantId
-      WHERE t3.usefulCount = max_count
+               JOIN temp1 t2
+                    ON t1.city = t2.city
+               JOIN Review t3
+                    ON t1.restaurantId = t3.restaurantId
+               JOIN RestaurantMedia RM 
+                    ON t1.restaurantId = RM.restaurantId
+      WHERE usefulCount = max_count
         AND t1.open = 'Y'
       ORDER BY t1.city
-      LIMIT 5
+      LIMIT 5;
       `;
 
       const bestSameCity = `
-      SELECT R.restaurantId,
-             R.restaurantName,
-             R.avgRating,
-             R.city
+      SELECT R.restaurantId, R.restaurantName, R.avgRating, R.city, RM.imageUrl
       FROM Restaurant R,
-           (SELECT temp.city,
-                   temp.state
-            FROM (SELECT S.restaurantId,
-                         R.avgRating,
-                         R.city,
-                         R.state
+           RestaurantMedia RM,
+           (SELECT city, state
+            FROM (SELECT S.restaurantId, avgRating, city, state
                   FROM SavedRestaurant S
-                           LEFT JOIN
-                       Restaurant R
-                       ON S.restaurantId = R.restaurantId
-                  WHERE (
-                      S.userId = ${userId}
-                      )
-                    AND (
-                          R.avgRating = (SELECT MAX(Restaurant.avgRating)
-                                         FROM Restaurant
-                                         WHERE EXISTS(
-                                                       SELECT 1
-                                                       FROM SavedRestaurant
-                                                       WHERE (
-                                                           SavedRestaurant.userId = ${userId}
-                                                           )
-                                                         AND (
-                                                           Restaurant.restaurantId = SavedRestaurant.restaurantId
-                                                           )
-                                                   ))
-                      )) temp
-            LIMIT 1) t2
+                           LEFT JOIN Restaurant R
+                                     ON S.restaurantId = R.restaurantId
+                  WHERE userId = ${userId}) temp
+            WHERE temp.avgRating = (SELECT MAX(avgRating)
+                                    FROM Restaurant
+                                    WHERE restaurantId in (SELECT restaurantId FROM SavedRestaurant WHERE userId = ${userId}))) t2
       WHERE R.city = t2.city
         AND R.state = t2.state
         AND R.reviewCount > 30
         AND R.open = 'Y'
-      ORDER BY R.avgRating DESC
-      LIMIT 5
+        AND R.restaurantId = RM.restaurantId
+      ORDER BY avgRating DESC
+      LIMIT 5;
       `;
 
       const recommender = {
